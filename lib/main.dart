3a120da9af99e8231e1666f07d6346c2ae06d730
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-09-19 19:43:37 -0400

    theme file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,12 +11,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme().theme(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: () {},
+            child: const Text('Click me')
+          ),
         ),
       ),
     );
