3b9f2a7b145afb933737c43a0bad50db99ef592b
EmmanuelJCA <emmanuelcanate@gmail.com>
2023-09-19 22:46:45 -0400

    chat screen

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://img.ecartelera.com/noticias/66000/66021-c.jpg'),
+          ),
+        ),
+        title: const Text('Mi amor 💜'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(itemBuilder: (context, index) {
+              return Text('Indice: $index');
+            })),
+            const Text('Hola mundo')
+          ],
+        ),
+      ),
+    );
+  }
+}
